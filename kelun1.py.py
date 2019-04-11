3960d8ae8a7f5f7f983536f0519a58543d0767c7
xianyu2006 <48900180+xianyu2006@users.noreply.github.com>
2019-04-11 20:44:03 +0800

    Update kelun1.py.py

--- kelun1.py.py
+++ kelun1.py.py
@@ -14,8 +14,8 @@ fp= open('113.csv','a+',errors='ignore',newline="",encoding='utf-8')
 writer = csv.writer(fp)
 writer.writerow(['名称','价格'])
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
-           'Cookie':'PHPSESSID=blf5ucnur5ldr1cvs12cmr4ots; UM_distinctid=169b4d7a3e738f-070df82c5473bd-5f123917-13c680-169b4d7a3e8245; CNZZDATA1275384522=1672243937-1553516935-null%7C1553516935; c_member_id=109224'
-}
+           'Cookie':}  #此处加上自己的cookie
+
 #for i in range(27):
 urls =["http://klyp.kelunyy.com/goods-filter-1247-0-0-0-0-1-{}.html".format(i) for i in range(1,27,1)]
 
